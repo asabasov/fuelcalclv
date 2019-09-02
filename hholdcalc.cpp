1a0da8d987a4e5705ce042a710cd21915ecfe251
asabasov <46406374+asabasov@users.noreply.github.com>
2019-09-02 16:20:23 +0300

    Monthly exp calc
    Continued developing

--- hholdcalc.cpp
+++ hholdcalc.cpp
@@ -7,10 +7,26 @@ hholdcalc::hholdcalc()
 {
     int salary;
     int flatexp;
+    int flatutilexp;
+    int fuelexp;
     cout << "Now we have to calculate the monthly house hold expenses!" << endl;
     cout << "Please enter your salary: ";
     cin >> salary;
     cout << "Your salary is " << salary << "EUR fixed." << endl;
     cout << "Please enter your house/flat rent expense: ";
     cin >> flatexp;
+    cout << "Please enter your house/flat utility expense: ";
+    cin >> flatutilexp;
+    cout << "Please enter your car lease expense: ";
+    cin >> fuelexp;
+    int formula = flatexp + flatutilexp + fuelexp;
+    cout << "Your monthly expense is " << formula << endl;
+    if (formula >= salary) {
+        cout << "You earn less than you spend!" << endl;
+        cout << "Please consider your spendings!" << endl;
+    } else {
+        cout << "Nice one!" << endl;
+        cout << "You are a financially responsible person!" << endl;
+    }
+    cout << "Your expenditures are " << formula % salary / 10 << "% of your salary!" << endl;
 }
